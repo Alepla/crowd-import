50340baf281c50e021ce2d8ea245e07818bcc1fe
Alepla <aleplacambra@gmail.com>
2025-11-21 23:00:01 +0100

    fix: remove invalid fastRefresh option from React plugin config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,22 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    open: true
+    open: true,
+    warmup: {
+      clientFiles: ['./src/main.tsx', './src/App.tsx'],
+    },
+  },
+  optimizeDeps: {
+    include: [
+      'react',
+      'react-dom',
+      'react-router-dom',
+      'react-intl',
+      'zustand',
+      'lodash',
+      'axios',
+    ],
+    exclude: [],
   },
   build: {
     minify: 'esbuild',
@@ -16,11 +31,11 @@ export default defineConfig({
         manualChunks: {
           'react-vendor': ['react', 'react-dom', 'react-router-dom'],
           'intl-vendor': ['react-intl'],
-          'utils-vendor': ['lodash', 'axios', 'zustand']
-        }
-      }
+          'utils-vendor': ['lodash', 'axios', 'zustand'],
+        },
+      },
     },
-    chunkSizeWarningLimit: 1000
-  }
+    chunkSizeWarningLimit: 1000,
+  },
 })
 
